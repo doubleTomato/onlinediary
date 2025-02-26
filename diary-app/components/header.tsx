6a4fd6e66e27aebc031ffd4e1841a0ed649b2b87
doubleTomato <doubleTomato21@gmail.com>
2025-02-26 22:08:21 +0900

    [fix] tailwindcss 적용안되는 오류 수정

--- diary-app/components/header.tsx
+++ diary-app/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
             <header className="bg-white shadow-md p-4 fixed w-full top-0 flex py-10">
-                 <div className="max-w-4xl mx-auto flex justify-between items-center h-[30px]">
+                 <div className="max-w-4xl mx-auto flex justify-between items-center h-10">
                        <a href="/" className="text-xl font-bold">DiaryApp</a>
                        <nav className="flex">
                          <a href="/dashboard" className="px-4">Dashboard</a>
